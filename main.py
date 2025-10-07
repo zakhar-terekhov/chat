c1e4120e3ed278edbd567be08e35862c43e825eb
zahar-terekhov <zaharterekhov666@yandex.ru>
2025-10-07 21:06:17 +0900

    Save all messages to file.

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from chat!")
+import asyncio
+
+
+async def main():
+    reader, _ = await asyncio.open_connection("minechat.dvmn.org", 5000)
+
+    while True:
+        data = await reader.read(100)
+        print(
+            data.decode(
+                encoding="utf-8",
+            )
+        )
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
